e2abf01690008d7ac58351a973dd3c93a2855495
fiverrforever0-spec <fiverrforever0@gmail.com>
2026-07-28 06:41:51 +0200

    Add GsmInCallService for call management
    Implement GsmInCallService to manage call states and audio bridging.

--- app/src/main/java/com/istithmarak/gateway/GsmInCallService.kt
+++ app/src/main/java/com/istithmarak/gateway/GsmInCallService.kt
@@ -1,1 +1,43 @@
+package com.istithmarak.gateway
+
+import android.telecom.Call
+import android.telecom.InCallService
+import android.util.Log
+
+class GsmInCallService : InCallService() {
+
+    companion object {
+        var activeCall: Call? = null
+        private const val TAG = "IstithmarakInCall"
+    }
+
+    override fun onCallAdded(call: Call) {
+        super.onCallAdded(call)
+        activeCall = call
+        Log.d(TAG, "Call Added Successfully. State: ${call.state}")
+        
+        call.registerCallback(object : Call.Callback() {
+            override fun onStateChanged(call: Call, newState: Int) {
+                super.onStateChanged(call, newState)
+                Log.d(TAG, "Call State Updated: $newState")
+                
+                if (newState == Call.STATE_ACTIVE) {
+                    AudioBridgeEngine.startBridge()
+                } else if (newState == Call.STATE_DISCONNECTED) {
+                    AudioBridgeEngine.stopBridge()
+                    activeCall = null
+                }
+            }
+        })
+    }
+
+    override fun onCallRemoved(call: Call) {
+        super.onCallRemoved(call)
+        if (activeCall == call) {
+            activeCall = null
+            AudioBridgeEngine.stopBridge()
+        }
+        Log.d(TAG, "Call Removed and Terminated")
+    }
+}
 
